047e37e47edb95efa9e4772cf9f08b57c4c87112
brunonunez-dev <260222547+brunonunez-dev@users.noreply.github.com>
2026-03-14 08:32:30 -0400

    refactor: implement session injection and auto-initialization to fix table error

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
-from src.database import SessionLocal
 from src.services import create_transaction_service
 from datetime import date
+from src.database import get_db, init_db
+
+init_db()
 
 st.set_page_config(page_title="SBM - Solid Budget Manager", layout="wide")
 
-db=SessionLocal()
 
 st.header("➕ Nova Transação")
 
@@ -27,20 +28,21 @@ with st.form("transaction_form", clear_on_submit=True):
     submit = st.form_submit_button("Salvar")
 
     if submit:
-        try:
-            new_tx = create_transaction_service(
-                db=db,
-                description=description,
-                value=value,
-                transaction_type=transaction_type,
-                category=category,
-                date_obj=date_obj
-            )
-            st.success(f"Transação '{description}' salva com sucesso!")
-            st.rerun()
-        except ValueError as e:
-            st.error(f"Erro de Validação: {e}")
-        except Exception as e:
-            st.error(f"Erro interno no sistema: {e}")
-        finally:
-            db.close()
+        with get_db() as db:
+            try:
+                new_tx = create_transaction_service(
+                    db=db,
+                    description=description,
+                    value=value,
+                    transaction_type=transaction_type,
+                    category=category,
+                    date_obj=date_obj
+                )
+                st.success(f"Transação '{description}' salva com sucesso!")
+                st.rerun()
+            except ValueError as e:
+                st.error(f"Erro de Validação: {e}")
+            except Exception as e:
+                st.error(f"Erro interno no sistema: {e}")
+            finally:
+                db.close()
